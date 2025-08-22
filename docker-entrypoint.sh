d3ad30bf1a83b45ff7e60c37e399afc31e2e9639
SPECTRE-PRO <0spectrep0@gmail.com>
2025-08-22 14:30:12 +0800

    done to add the key

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 set -e
-app_command=("$@")
+API_KEY_ARG=""
 if [ -n "${API_KEY}" ]; then
   echo "偵測到 API_KEY。將以安全模式啟動，並注入 API 金鑰。"
-  app_command+=("--api-key" "${API_KEY}")
+  API_KEY_ARG="--api-key ${API_KEY}"
 else
   echo "未偵測到 API_KEY。將以普通模式啟動，不注入 API 金鑰。"
 fi
-echo "最終執行命令: ${app_command[@]}"
-echo "----------------------"
-exec "${app_command[@]}"
+echo "最終執行命令: $@ $API_KEY_ARG"
+exec "$@" $API_KEY_ARG
